0b515b584a217443f1fabe1ea2b6143ceaaf221d
Tejinder-Kaur1 <kaurtejinder540@gmail.com>
2026-05-09 19:15:26 +0530

    Name Changes

--- footer.php
+++ footer.php
@@ -96,7 +96,7 @@
 
                <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                Designed By
-               <span class="copyright-owner">Tejinder & Jasmine & Dharampal</span>
+               <span class="copyright-owner">Tejinder </span>
                <br /><br />
                <!-- Distributed By
                <a
